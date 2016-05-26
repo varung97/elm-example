02bb56377b565d76c57140508cf018f92bff9525
varung97 <varung97@yahoo.co.in>
2016-05-26 20:22:44 +0800

    Added Websocket example for 0.16, renamed time.elm files

--- 0.16/websocket-0.16.elm
+++ 0.16/websocket-0.16.elm
@@ -1,35 +1,57 @@
-module Example where
-
-import Graphics.Element exposing (show)
+import Graphics.Element exposing (..)
 import Task exposing (Task, andThen)
 import String
 import Time
 import SocketIO
+import Text
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 
+eventName = "echo"
+type Action = Send | NoOp
+
+-- Fails with type x and succeeds with type SocketIO.Socket
 socket : Task x SocketIO.Socket
 socket = SocketIO.io "http://localhost:8001" SocketIO.defaultOptions
 
-eventName = "example"
+-- port initial : Task x ()
+-- port initial = socket `andThen` SocketIO.emit eventName "Hi"
 
--- send a value once at program start
-port initial : Task x ()
-port initial = socket `andThen` SocketIO.emit eventName "0"
-
+-- Creates a signal mailbox, with default value null, to which the reponses will
+-- be sent
 received : Signal.Mailbox String
 received = Signal.mailbox "null"
 
 -- set up the receiving of data
+-- Server emits on same event name (contrived) and this is sent to the mailbox
 port responses : Task x ()
 port responses = socket `andThen` SocketIO.on eventName received.address
 
-validResponses : Signal Int
-validResponses =
-    Signal.filterMap (String.toInt>>Result.toMaybe) 0 received.signal
+-- Create a signal mailbox with default value NoOp which receives the commands
+-- to send message
+sendMessageMailbox : Signal.Mailbox Action
+sendMessageMailbox = Signal.mailbox NoOp
 
--- send many values (with a 1-second throttle)
-port recurring : Signal (Task x ())
-port recurring =
-    Signal.map (\i -> socket `andThen` SocketIO.emit eventName (toString (i+1)))
-    <| Signal.sampleOn (Time.fps 1) validResponses
+-- convert the action to a string to be sent
+action : Action -> String
+action act =
+  case act of
+    Send -> "Echo"
+    NoOp -> "null"
 
-main = Signal.map show validResponses
+-- convert the actions to a task so that message is sent to the websocket
+port sendMessage : Signal (Task x ())
+port sendMessage = Signal.map (\act -> socket `andThen` SocketIO.emit eventName (action act)) sendMessageMailbox.signal
+
+view : String -> Html
+view str =
+  let
+    txt = fromElement <| container 1000 500 middle <| centered <| Text.height 40 <| Text.fromString <| str
+    -- onClick sends an action "Send" to the mailbox specified
+    but = button [onClick sendMessageMailbox.address Send] [text "Send Message"]
+  in
+    div [] [txt, but]
+
+main : Signal Html
+main = Signal.map view received.signal
